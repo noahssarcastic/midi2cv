1fe2ca880b0b2757d37489d3604ad1f8c213fd76
Noah Iniguez <niniguezg@gmail.com>
2024-05-23 11:32:34 -0500

    Update test program with midi controls

--- blink.cpp
+++ blink.cpp
@@ -1,34 +1,95 @@
 #include "daisy_seed.h"
 
-// Use the daisy namespace to prevent having to type
-// daisy:: before all libdaisy functions
 using namespace daisy;
 
-// Declare a DaisySeed object called hardware
-DaisySeed hardware;
+DaisySeed hw;
+MidiUartHandler midi;
+
+void GetMidiTypeAsString(MidiEvent &msg, char *str)
+{
+    switch (msg.type)
+    {
+    case NoteOff:
+        strcpy(str, "NoteOff");
+        break;
+    case NoteOn:
+        strcpy(str, "NoteOn");
+        break;
+    case PolyphonicKeyPressure:
+        strcpy(str, "PolyKeyPres.");
+        break;
+    case ControlChange:
+        strcpy(str, "CC");
+        break;
+    case ProgramChange:
+        strcpy(str, "Prog. Change");
+        break;
+    case ChannelPressure:
+        strcpy(str, "Chn. Pressure");
+        break;
+    case PitchBend:
+        strcpy(str, "PitchBend");
+        break;
+    case SystemCommon:
+        strcpy(str, "Sys. Common");
+        break;
+    case SystemRealTime:
+        strcpy(str, "Sys. Realtime");
+        break;
+    case ChannelMode:
+        strcpy(str, "Chn. Mode");
+        break;
+    default:
+        strcpy(str, "Unknown");
+        break;
+    }
+}
 
 int main(void)
 {
     // Declare a variable to store the state we want to set for the LED.
     bool led_state;
-    led_state = true;
+    led_state = false;
 
-    // Configure and Initialize the Daisy Seed
-    // These are separate to allow reconfiguration of any of the internal
-    // components before initialization.
-    hardware.Configure();
-    hardware.Init();
+    /** Initialize our hardware */
+    hw.Init();
 
-    // Loop forever
-    for (;;)
+    hw.StartLog();
+
+    MidiUartHandler::Config midi_config;
+    midi.Init(midi_config);
+
+    midi.StartReceive();
+
+    /** Infinite Loop */
+    while (1)
     {
         // Set the onboard LED
-        hardware.SetLed(led_state);
+        hw.SetLed(led_state);
 
-        // Toggle the LED state for the next time around.
-        led_state = !led_state;
+        /** Process MIDI in the background */
+        midi.Listen();
 
-        // Wait 500ms
-        System::Delay(500);
+        /** Loop through any MIDI Events */
+        while (midi.HasEvents())
+        {
+            MidiEvent msg = midi.PopEvent();
+
+            switch (msg.type)
+            {
+            case NoteOn:
+            {
+                led_state = true;
+            }
+            break;
+            case NoteOff:
+            {
+                led_state = false;
+            }
+            break;
+            default:
+                break;
+            }
+        }
     }
 }
