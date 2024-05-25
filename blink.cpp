9a5291ef2ce1995c67a1c2445748ecb3cf3599ab
Noah Iniguez <niniguezg@gmail.com>
2024-05-25 11:10:27 -0500

    Add DAC out for pitch

--- blink.cpp
+++ blink.cpp
@@ -6,78 +6,44 @@ using namespace daisy;
 DaisySeed hw;
 MidiUartHandler midi;
 
-void GetMidiTypeAsString(MidiEvent &msg, char *str)
-{
-    switch (msg.type)
-    {
-    case NoteOff:
-        strcpy(str, "NoteOff");
-        break;
-    case NoteOn:
-        strcpy(str, "NoteOn");
-        break;
-    case PolyphonicKeyPressure:
-        strcpy(str, "PolyKeyPres.");
-        break;
-    case ControlChange:
-        strcpy(str, "CC");
-        break;
-    case ProgramChange:
-        strcpy(str, "Prog. Change");
-        break;
-    case ChannelPressure:
-        strcpy(str, "Chn. Pressure");
-        break;
-    case PitchBend:
-        strcpy(str, "PitchBend");
-        break;
-    case SystemCommon:
-        strcpy(str, "Sys. Common");
-        break;
-    case SystemRealTime:
-        strcpy(str, "Sys. Realtime");
-        break;
-    case ChannelMode:
-        strcpy(str, "Chn. Mode");
-        break;
-    default:
-        strcpy(str, "Unknown");
-        break;
-    }
-}
+const float PEAK_PITCH_VOLTAGE = 3.3f;
 
 int main(void)
 {
-    // Declare a variable to store the state we want to set for the LED.
-    bool led_state;
-    led_state = false;
-
-    /** Initialize our hardware */
+    // Daisy Seed config
     hw.Init();
 
-    hw.StartLog();
+    // DAC config
+    DacHandle::Config dac_cfg;
+    dac_cfg.bitdepth = DacHandle::BitDepth::BITS_12;
+    dac_cfg.buff_state = DacHandle::BufferState::ENABLED;
+    dac_cfg.mode = DacHandle::Mode::POLLING;
+    dac_cfg.chn = DacHandle::Channel::ONE;
+    hw.dac.Init(dac_cfg);
 
+    // Midi Receiver config
     MidiUartHandler::Config midi_config;
     midi.Init(midi_config);
-
     midi.StartReceive();
 
+    // Debug LED
+    bool led_state = false;
+
+    // Store NoteOnEvent info for multiple key presses
     std::vector<NoteOnEvent> notes_pressed;
 
-    /** Infinite Loop */
+    int pitch = 0;
+
     while (1)
     {
-        // Set the onboard LED
         hw.SetLed(led_state);
+        hw.dac.WriteValue(DacHandle::Channel::ONE, pitch);
 
-        /** Process MIDI in the background */
         midi.Listen();
-
-        /** Loop through any MIDI Events (ignoring channel for now) */
+        // Loop through any MIDI Events (ignoring channel for now)
         while (midi.HasEvents())
         {
             MidiEvent msg = midi.PopEvent();
-
             switch (msg.type)
             {
             case NoteOn:
@@ -93,8 +59,38 @@ int main(void)
             default:
                 break;
             }
+        }
 
-            led_state = !notes_pressed.empty();
+        led_state = !notes_pressed.empty();
+
+        if (!notes_pressed.empty())
+        {
+            NoteOnEvent last_pressed = notes_pressed.back();
+
+            /* Calculate note voltage (v/oct)
+            Midi middle C is 60, let's center around that.
+            Take the note offset from center, multiply by 1/12, and offset by
+            the center of our V range [0,PEAK_PITCH_VOLTAGE]. */
+            float note_voltage = (last_pressed.note - 60) * (1.0f / 12.0f) + PEAK_PITCH_VOLTAGE / 2.0f;
+
+            /* Convert V ([0,PEAK_PITCH_VOLTAGE]) to DAC ([0, 4095])
+            For intervals [a,b] and [c,d]:
+            f(x) = c + (d - c) / (b - a) * (x - a); */
+            pitch = int(4095 / PEAK_PITCH_VOLTAGE * note_voltage);
+
+            // Clamp pitch between [0,4095]
+            if (pitch > 4095)
+            {
+                pitch = 4095;
+            }
+            else if (pitch < 0)
+            {
+                pitch = 0;
+            }
+        }
+        else
+        {
+            pitch = 0;
         }
     }
 }
